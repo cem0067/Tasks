cb3a7c386021188a8cb4f7f60602d9e049f7f8b3
cem0067 <cem0067@mix.wvu.edu>
2022-01-12 13:53:55 -0500

    Courtney Task 01 upload b

--- Task_01/Task_01.r
+++ Task_01/Task_01.r
@@ -1,3 +1,29 @@
 # pw: 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
 #install.packages("swirl")
 library(swirl)
+
+swirl()
+Courtney
+1
+1
+5 + 7
+x <- 5+7
+x
+y <- x-3
+y
+z <- c(1.1, 9, 3.14)
+?c
+z
+c(z, 555, z)
+z * 2 +100
+my_sqrt <- sqrt(z-1)
+1
+my_sqrt
+my_div <- z / my_sqrt
+3
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+z * 2 + 1000
+my_div
+1
