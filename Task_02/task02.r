bac329119822c12372fadcef1bb665d5ac3a43c5
cem0067 <cem0067@mix.wvu.edu>
2022-01-23 16:43:35 -0500

    Courtney Task 02 upload b

--- Task_02/task02.r
+++ Task_02/task02.r
@@ -5,6 +5,7 @@ write.csv(Data, 'rawdata.csv', quote=F)
 length(Data)
 nrow(Data)
 ncol(Data)
+colnames(Data)
 head(Data)
 Data[1,]
 Data[2,]
@@ -18,7 +19,7 @@ Feeds <- which(Data[,'event'] == 'bottle')
 Feeds <- which(Data$event == 'bottle')
 dayID <- apply(Data, 1, function(x) paste(x[1:3], collapse='-'))
 dateID <- sapply(dayID, as.Date, format = "%Y-%m-%d", origin = "2019-04-18")
-Data$age <- dateID[which(Data$event == 'birth')]
+Data$age <- dateID - dateID[which(Data$event == 'birth')]
 head(Data)
 beren2 <- Data
 beren3 <- beren2[order(beren2$age),]
