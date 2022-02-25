c2a7aa361a20061be4f369c77daef6df6cb3c665
cem0067 <cem0067@mix.wvu.edu>
2022-02-24 20:01:42 -0500

    Courtney Final Project part 1

---
+++ Final_Project/hypothesis.r
@@ -0,0 +1,3 @@
+#The hypothesis that I am going to test is that the difference in the spider's condition (abdomen width compared to carapace width) from right before the trial and the isolation period causes a decrease in the Maximum Prey-to-Predator Size Ratio.
+setwd('~/Desktop/Evolution/Final_Project')
+read.csv("Data Set 1.csv", stringsAsFactors = F)
